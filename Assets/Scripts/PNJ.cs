b67380d40f1aa5378908a1a4772fca4254ab9586
DESKTOP-N4F36O6\roylo <roylouis0424@gmail.com>
2024-05-26 14:21:30 +0200

    Pnj fini aleluia

--- Assets/Scripts/PNJ.cs
+++ Assets/Scripts/PNJ.cs
@@ -10,6 +10,7 @@ public class PNJ : MonoBehaviour
     public Button ButtonAccpet;
     public Button ButtonExit;
     public TextMeshProUGUI TextComponent;
+    public TextMeshProUGUI TextComponent2;
     public Image ImageText;
 
     private int index;
@@ -23,6 +24,7 @@ public class PNJ : MonoBehaviour
         TextComponent.gameObject.SetActive(true);
 
         TextComponent.text = string.Empty;
+        TextComponent2.text = string.Empty;
         StartDialogue();
     }
 
@@ -43,5 +45,48 @@ public class PNJ : MonoBehaviour
         }
         ButtonAccpet.gameObject.SetActive(true);
         ButtonExit.gameObject.SetActive(true);
+    }
+
+    public void OnClickAccpet()
+    {
+        if(MainGame.Instance._PlayerStats.Gold >= 5)
+        {
+            MainGame.Instance._PlayerStats.Gold -= 5;
+            Debug.Log("Tu a acheter la potion");
+        }
+        else
+        {
+            TextComponent.gameObject.SetActive(false);
+            Debug.Log("Ta pas assez de thune");
+            index =1;
+            TextComponent2.gameObject.SetActive(true);
+            StartCoroutine (TypeLine2());
+        }
+        
+        ButtonAccpet.gameObject.SetActive(false);
+        
+
+
+    }
+
+    IEnumerator TypeLine2()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            TextComponent2.text += c;
+            yield return new WaitForSeconds(TextSpeed);
+
+        }
+        
+        ButtonExit.gameObject.SetActive(true);
+    }
+
+    public void OnClickExit()
+    {
+        ImageText.gameObject.SetActive(false);
+        TextComponent.gameObject.SetActive(false);
+        ButtonExit.gameObject.SetActive(false); 
+        ButtonAccpet.gameObject.SetActive(false);
+        TextComponent2.gameObject.SetActive(false);
     }
 }
